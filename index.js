66409d54e913b05e94bd8edbb8aa15fb5333d984
mostafachowdhury1 <mosstafachowdhury@gmail.com>
2022-11-03 19:50:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require('express')
+const app=express()
+const cors=require('cors')
+const Port=process.env.PORT || 5000;
+
+app.use(cors())
+const cetagory=require('./data/cetagory.json')
+const news=require('./data/news.json')
+const ness=require('./data/ness.json')
+app.get("/Newscetagory",(req,res)=>{
+    res.send(cetagory)
+})
+
+
+app.get("/ness",(req,res)=>{
+    res.send(ness)
+})
+
+
+app.get('/ness/:id',(req,res)=>{
+    const id=req.params.id;
+    const allnews=ness.find(n=>n.id==id)
+    res.send(allnews)
+})
+
+app.get("/news",(req,res)=>{
+    res.send(news)
+})
+app.get('/cetagory/:id',(req,res)=>{
+    const id=req.params.id;
+    const allcetagory=news.filter(n=>n.category_id===id)
+    res.send(allcetagory)
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id=req.params.id;
+    const allnews=news.find(n=>n.id==id)
+    res.send(allnews)
+})
+
+app.get("/",(req,res)=>{
+    res.send("heyy")
+})
+
+app.listen(Port,()=>{
+ console.log("run is",Port)
+})
+
+
+
+module.exports = app;
